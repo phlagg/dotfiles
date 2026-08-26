7db31408f9636117b08811225e951192b95b1086
Michael Woods <michaelthomaswoods@gmail.com>
2026-08-25 19:43:25 -0700

    updated

--- hypr/.config/hypr/conf/workspaces/default.lua
+++ hypr/.config/hypr/conf/workspaces/default.lua
@@ -1,11 +1,10 @@
 
 hl.workspace_rule({
-    workspace = "1",
+    workspace = "3",
     monitor = "DP-2",
-    default = true
 })
 hl.workspace_rule({
-    workspace = "2",
+    workspace = "4",
     monitor = "DP-2"
 })
 -- hl.workspace_rule({
@@ -18,12 +17,12 @@ hl.workspace_rule({
 -- })
 
 hl.workspace_rule({
-    workspace = "3",
+    workspace = "1",
     monitor = "HDMI-A-1",
     default = true
 })
 hl.workspace_rule({
-    workspace = "4",
+    workspace = "2",
     monitor = "HDMI-A-1"
 })
 -- hl.workspace_rule({
